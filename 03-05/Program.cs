c0edd2fe12bc073a79ef47fe21a85ee0028b6fbc
cleasholdorf <you@example.com>
2022-05-03 19:05:13 -0300

    começo

---
+++ 03-05/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace _03_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ConnetionString = "Data Source=DESKTOP-25SHDUN\\SQLEXPRESS;Initial Catalog=c#;Integrated Security=true;";
+           SqlConnection sqlConnection = new SqlConnection(ConnetionString);
+            sqlConnection.Open();
+
+        }
+    }
+}
